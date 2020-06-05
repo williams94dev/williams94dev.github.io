04bf86b59be1c70a1781bfc11e2cd2b5d5ffd7c6
williams94dev <williams.guti.dev@gmail.com>
2020-06-05 15:57:18 -0400

    Desarrollo frontend aplicacion para examen final

---
+++ precache-manifest.b3780357749b7d89b88f63ee522675d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c92e607f30780b8d04e72df8070586f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7c66c4aa3f1f3f9d439",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "77e78f960110cfc3a85b",
+    "url": "/static/css/main.b32fc293.chunk.css"
+  },
+  {
+    "revision": "e7c66c4aa3f1f3f9d439",
+    "url": "/static/js/2.a375f01b.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/static/js/2.a375f01b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e78f960110cfc3a85b",
+    "url": "/static/js/main.be7ef8d9.chunk.js"
+  },
+  {
+    "revision": "8182a387c804e86d8e73",
+    "url": "/static/js/runtime-main.b810a271.js"
+  }
+]);
